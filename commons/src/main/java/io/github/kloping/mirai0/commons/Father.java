934189f718893aa51a649511bc4656a9d45b596d
HRS-Computer <3474006766@qq.com>
2022-03-23 13:19:31 +0800

    update

--- commons/src/main/java/io/github/kloping/mirai0/commons/Father.java
+++ commons/src/main/java/io/github/kloping/mirai0/commons/Father.java
@@ -7,6 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author github.kloping
  */
@@ -16,8 +19,37 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class Father {
     public static final String ALL = "all";
+    public static final String SPLIT = ",";
 
     @TableId(type = IdType.ASSIGN_UUID)
     private Long id;
     private String permission;
+
+    public Set<Long> getGids() {
+        Set<Long> set = new HashSet<>();
+        for (String s : permission.split(SPLIT)) {
+            if (s.trim().isEmpty()) continue;
+            set.add(Long.parseLong(s.trim()));
+        }
+        return set;
+    }
+
+    public boolean hasPermission(long gid) {
+        return getGids().contains(gid);
+    }
+
+    public String addPermission(long gid) {
+        Set<Long> set = getGids();
+        set.add(gid);
+        for (Long aLong : set) {
+            permission = permission + SPLIT + aLong;
+        }
+        if (permission.startsWith(SPLIT)) {
+            permission = permission.substring(1);
+        }
+        if (permission.equals(SPLIT)) {
+            permission = permission.substring(0, permission.length() - 1);
+        }
+        return permission;
+    }
 }
