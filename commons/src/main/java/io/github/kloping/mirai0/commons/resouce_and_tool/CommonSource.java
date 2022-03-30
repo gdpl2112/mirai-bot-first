d54a301bffd51019ff31f47ab53678ed08dce017
HRS-Computer <3474006766@qq.com>
2022-03-30 09:51:49 +0800

    update

--- commons/src/main/java/io/github/kloping/mirai0/commons/resouce_and_tool/CommonSource.java
+++ commons/src/main/java/io/github/kloping/mirai0/commons/resouce_and_tool/CommonSource.java
@@ -52,4 +52,20 @@ public class CommonSource {
         int v3 = (int) dv3;
         return v3;
     }
+
+    /**
+     * 将指定数字转为指定位数字符
+     * (2,9)  => 09
+     *
+     * @param i
+     * @param value
+     * @return
+     */
+    public static String toStr(int i, int value) {
+        String s0 = Integer.toString(value);
+        while (s0.length() < i) {
+            s0 = "0" + s0;
+        }
+        return s0;
+    }
 }
