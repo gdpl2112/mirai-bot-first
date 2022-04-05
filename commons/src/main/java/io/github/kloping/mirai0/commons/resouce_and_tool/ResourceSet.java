86c9fee8325804bedf5fe695bd892ff2e94896fa
HRS-Computer <3474006766@qq.com>
2022-04-05 17:15:09 +0800

    update

--- commons/src/main/java/io/github/kloping/mirai0/commons/resouce_and_tool/ResourceSet.java
+++ commons/src/main/java/io/github/kloping/mirai0/commons/resouce_and_tool/ResourceSet.java
@@ -126,6 +126,7 @@ public final class ResourceSet {
         public static final String ATTACK_BREAK = "攻击被打断";
         public static final String BG_TIPS = "闭关中..";
         public static final String PLAYER_BG_TIPS = "玩家正在闭关中...";
+        public static final String FUNCTION_CLOSEING_TIPS = "功能暂时关闭";
     }
 
     public static final class FinalFormat {
