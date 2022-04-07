5ababd0d79979ba3ea59d0e9046dcf704eb06cdd
HRS-Computer <3474006766@qq.com>
2022-04-08 01:08:57 +0800

    update

---
+++ test-module/src/main/java/io/github/klopin/test0/kp/entity/pre/Chapter.java
@@ -0,0 +1,84 @@
+package io.github.klopin.test0.kp.entity.pre;
+
+public class Chapter {
+	private Number chapter;
+	private Number count;
+	private Number id;
+	private Number section1;
+	private String title;
+	private Number section2;
+	private Number section3;
+	private Number section4;
+
+	public Number getChapter(){
+		return this.chapter;
+	}
+
+	public Chapter setChapter(Number chapter) {
+		this.chapter = chapter;
+		return this;
+	}
+
+	public Number getCount(){
+		return this.count;
+	}
+
+	public Chapter setCount(Number count) {
+		this.count = count;
+		return this;
+	}
+
+	public Number getId(){
+		return this.id;
+	}
+
+	public Chapter setId(Number id) {
+		this.id = id;
+		return this;
+	}
+
+	public Number getSection1(){
+		return this.section1;
+	}
+
+	public Chapter setSection1(Number section1) {
+		this.section1 = section1;
+		return this;
+	}
+
+	public String getTitle(){
+		return this.title;
+	}
+
+	public Chapter setTitle(String title) {
+		this.title = title;
+		return this;
+	}
+
+	public Number getSection2(){
+		return this.section2;
+	}
+
+	public Chapter setSection2(Number section2) {
+		this.section2 = section2;
+		return this;
+	}
+
+	public Number getSection3(){
+		return this.section3;
+	}
+
+	public Chapter setSection3(Number section3) {
+		this.section3 = section3;
+		return this;
+	}
+
+	public Number getSection4(){
+		return this.section4;
+	}
+
+	public Chapter setSection4(Number section4) {
+		this.section4 = section4;
+		return this;
+	}
+}
