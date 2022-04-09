66ea9028eb42abf3234b1411230b39121c58a44d
HRS-Computer <3474006766@qq.com>
2022-04-09 20:05:09 +0800

    update

--- commons/src/main/java/io/github/kloping/mirai0/commons/resouce_and_tool/ResourceSet.java
+++ commons/src/main/java/io/github/kloping/mirai0/commons/resouce_and_tool/ResourceSet.java
@@ -154,7 +154,7 @@ public final class ResourceSet {
         public static final String HL_NOT_ENOUGH_TIPS = "魂力不足:您当前的魂力值=>%s%%";
         public static final String AT_FORMAT = "[@%s]";
         public static final String EARNINGS_TIPS_FORMAT = "该周收益\n收益:%s\n损失:%s";
-
+        public static final String WHERE_MEMBER_IS_MY_WIFE = "今天你的群友老婆是\n <Pic:http://q2.qlogo.cn/headimg_dl?dst_uin=%s&spec=100> 【%s】(%s) ";
     }
 
     public static final class FinalValue {
