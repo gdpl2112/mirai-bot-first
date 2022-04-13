12e35c8dd58615952b0d5fc26f3df613c67785b9
HRS-Computer <3474006766@qq.com>
2022-04-13 21:57:02 +0800

    update

---
+++ commons/src/main/java/io/github/kloping/mirai0/commons/apiEntitys/qingyunke/QingYunKeData.java
@@ -0,0 +1,28 @@
+package io.github.kloping.mirai0.commons.apiEntitys.qingyunke;
+
+/**
+ * @author github-kloping
+ */
+public class QingYunKeData
+{
+    private Number result;
+    private String content;
+
+    public Number getResult() {
+        return this.result;
+    }
+
+    public QingYunKeData setResult(Number result) {
+        this.result = result;
+        return this;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public QingYunKeData setContent(String content) {
+        this.content = content;
+        return this;
+    }
+}
