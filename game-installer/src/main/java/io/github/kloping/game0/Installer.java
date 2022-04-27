088e39b984dacede94d493b6200b10cd1e5622af
HRS-Computer <3474006766@qq.com>
2022-04-27 09:30:40 +0800

    update

--- game-installer/src/main/java/io/github/kloping/game0/Installer.java
+++ game-installer/src/main/java/io/github/kloping/game0/Installer.java
@@ -2,18 +2,14 @@ package io.github.kloping.game0;
 
 import java.io.File;
 
-import static io.github.kloping.game0.ClassPathJarLoader.loadLibs;
-import static io.github.kloping.game0.ResourceLoader.loadResource;
 
 /**
  * @author github.kloping
  */
 public class Installer {
+
     public static void main(String[] args) {
-        File file = new File("D:\\Projects\\OwnProjects\\MyMirai_01\\game-installer\\space");
-        loadLibs(new File(file, "classpath").getAbsolutePath());
-        loadResource(file.getAbsolutePath());
-        loadSrc(file);
+
     }
 
     private static void loadSrc(File file) {
