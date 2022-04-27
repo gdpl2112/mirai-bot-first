c589f263c99348d0a94a78583af886849198ff57
HRS-Computer <3474006766@qq.com>
2022-04-27 09:32:42 +0800

    update

--- game-installer/src/main/java/io/github/kloping/game0/Installer.java
+++ game-installer/src/main/java/io/github/kloping/game0/Installer.java
@@ -1,17 +1,10 @@
 package io.github.kloping.game0;
 
-import java.io.File;
-
-
 /**
  * @author github.kloping
  */
 public class Installer {
-
     public static void main(String[] args) {
 
-    }
-
-    private static void loadSrc(File file) {
     }
 }
