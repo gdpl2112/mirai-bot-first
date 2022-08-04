dfeb7679943023efdc944eef0d1eae5b04c3d662
HRS-Computer <3474006766@qq.com>
2022-08-04 16:40:39 +0800

    update

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- 主机:                           localhost
--- 服务器版本:                        5.5.68-MariaDB - MariaDB Server
--- 服务器操作系统:                      Linux
+-- 主机:                           127.0.0.1
+-- 服务器版本:                        5.5.47-MariaDB - mariadb.org binary distribution
+-- 服务器操作系统:                      Win64
 -- HeidiSQL 版本:                  9.1.0.4867
 -- --------------------------------------------------------
 
@@ -10,9 +10,50 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
 /*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
 
--- 导出 gdb1 的数据库结构
+-- 导出 mydb1 的数据库结构
 
--- 导出  表 aq_bag 结构
+-- 导出  表 mydb1.achievement 结构
+CREATE TABLE IF NOT EXISTS `achievement`
+(
+    `id`   int(11)    NOT NULL AUTO_INCREMENT,
+    `aid`  int(11)    NOT NULL COMMENT '成就id',
+    `qid`  bigint(20) NOT NULL COMMENT '领取人id',
+    `time` bigint(20) NOT NULL COMMENT '时间',
+    PRIMARY KEY (`id`),
+    KEY `id` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='成就领取记录';
+
+-- 正在导出表  mydb1.achievement 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `achievement`
+    DISABLE KEYS */;
+/*!40000 ALTER TABLE `achievement`
+    ENABLE KEYS */;
+
+
+-- 导出  表 mydb1.all_message 结构
+CREATE TABLE IF NOT EXISTS `all_message`
+(
+    `time`        bigint(18)   NOT NULL,
+    `id`          int(9)       NOT NULL,
+    `internal_id` int(9)       NOT NULL,
+    `sender_id`   bigint(18)   NOT NULL,
+    `bot_id`      bigint(18)   NOT NULL,
+    `type`        varchar(255) NOT NULL,
+    `from_id`     bigint(18)   NOT NULL,
+    `content`     varchar(255) NOT NULL,
+    `recalled`    int(9)       NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+-- 正在导出表  mydb1.all_message 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `all_message`
+    DISABLE KEYS */;
+/*!40000 ALTER TABLE `all_message`
+    ENABLE KEYS */;
+
+
+-- 导出  表 mydb1.aq_bag 结构
 CREATE TABLE IF NOT EXISTS `aq_bag`
 (
     `id`    int(11)    NOT NULL AUTO_INCREMENT,
@@ -24,43 +65,37 @@ CREATE TABLE IF NOT EXISTS `aq_bag`
     PRIMARY KEY (`id`),
     KEY `id` (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 218
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  aq_bag 的数据：~217 rows (大约)
-DELETE
-FROM `aq_bag`;
+-- 正在导出表  mydb1.aq_bag 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `aq_bag`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `aq_bag`
     ENABLE KEYS */;
 
 
--- 导出  表 auto_reply 结构
+-- 导出  表 mydb1.auto_reply 结构
 CREATE TABLE IF NOT EXISTS `auto_reply`
 (
-    `id`          int(11)                             NOT NULL AUTO_INCREMENT,
-    `k`           varchar(1024) CHARACTER SET utf8mb4 NOT NULL DEFAULT '0',
-    `v`           varchar(1024) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
-    `who`         varchar(50)                         NOT NULL,
-    `time`        varchar(50)                         NOT NULL,
-    `delete_stat` int(1) unsigned zerofill            NOT NULL,
+    `id`          int(11)                  NOT NULL AUTO_INCREMENT,
+    `k`           varchar(1024)            NOT NULL DEFAULT '0',
+    `v`           varchar(1024)            NOT NULL DEFAULT '',
+    `who`         varchar(50)              NOT NULL,
+    `time`        varchar(50)              NOT NULL,
+    `delete_stat` int(1) unsigned zerofill NOT NULL,
     PRIMARY KEY (`id`),
     KEY `id` (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 241
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  auto_reply 的数据：~151 rows (大约)
-DELETE
-FROM `auto_reply`;
+-- 正在导出表  mydb1.auto_reply 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `auto_reply`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `auto_reply`
     ENABLE KEYS */;
 
 
--- 导出  表 bag 结构
+-- 导出  表 mydb1.bag 结构
 CREATE TABLE IF NOT EXISTS `bag`
 (
     `id`    int(11)     NOT NULL AUTO_INCREMENT COMMENT '主键id',
@@ -72,35 +107,54 @@ CREATE TABLE IF NOT EXISTS `bag`
     PRIMARY KEY (`id`),
     KEY `id` (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 47055
   DEFAULT CHARSET = utf8 COMMENT ='玩家背包库';
 
--- 正在导出表  bag 的数据：~27,183 rows (大约)
-DELETE
-FROM `bag`;
+-- 正在导出表  mydb1.bag 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `bag`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `bag`
     ENABLE KEYS */;
 
 
--- 导出  表 father 结构
+-- 导出  表 mydb1.comment 结构
+CREATE TABLE IF NOT EXISTS `comment`
+(
+    `comment_id` int(9)       NOT NULL AUTO_INCREMENT,
+    `notice_id`  int(9)       NOT NULL,
+    `time`       bigint(18)   NOT NULL,
+    `nick_name`  varchar(255) NOT NULL,
+    `icon`       varchar(255) NOT NULL,
+    `content`    varchar(255) NOT NULL,
+    `state`      int(9)       NOT NULL,
+    PRIMARY KEY (`comment_id`),
+    KEY `comment_id` (`comment_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+-- 正在导出表  mydb1.comment 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `comment`
+    DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment`
+    ENABLE KEYS */;
+
+
+-- 导出  表 mydb1.father 结构
 CREATE TABLE IF NOT EXISTS `father`
 (
     `id`         bigint(20)  NOT NULL,
-    `permission` varchar(50) NOT NULL DEFAULT 'all',
+    `permission` varchar(50) NOT NULL DEFAULT '0',
     UNIQUE KEY `id` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  father 的数据：~42 rows (大约)
-DELETE
-FROM `father`;
+-- 正在导出表  mydb1.father 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `father`
+    DISABLE KEYS */;
 /*!40000 ALTER TABLE `father`
     ENABLE KEYS */;
 
 
--- 导出  表 group_conf 结构
+-- 导出  表 mydb1.group_conf 结构
 CREATE TABLE IF NOT EXISTS `group_conf`
 (
     `id`      bigint(18) DEFAULT NULL,
@@ -113,16 +167,14 @@ CREATE TABLE IF NOT EXISTS `group_conf`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  group_conf 的数据：~9 rows (大约)
-DELETE
-FROM `group_conf`;
+-- 正在导出表  mydb1.group_conf 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `group_conf`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `group_conf`
     ENABLE KEYS */;
 
 
--- 导出  表 g_info 结构
+-- 导出  表 mydb1.g_info 结构
 CREATE TABLE IF NOT EXISTS `g_info`
 (
     `qid`          bigint(20) NOT NULL,
@@ -134,20 +186,23 @@ CREATE TABLE IF NOT EXISTS `g_info`
     `diedc`        int(11)    NOT NULL,
     `useskillc`    int(11)    NOT NULL,
     `star`         int(11)    NOT NULL,
+    `reqc`         int(11)    NOT NULL COMMENT '请求支援次数',
+    `helpc`        int(11)    NOT NULL COMMENT '支援次数',
+    `buyc`         int(11)    NOT NULL COMMENT '购买次数',
+    `salec`        int(11)    NOT NULL COMMENT '出售次数',
+    `ftc`          int(11)    NOT NULL COMMENT '完成任务次数',
     UNIQUE KEY `qid` (`qid`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  g_info 的数据：~233 rows (大约)
-DELETE
-FROM `g_info`;
+-- 正在导出表  mydb1.g_info 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `g_info`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `g_info`
     ENABLE KEYS */;
 
 
--- 导出  表 hhpz 结构
+-- 导出  表 mydb1.hhpz 结构
 CREATE TABLE IF NOT EXISTS `hhpz`
 (
     `id`    int(11)    NOT NULL AUTO_INCREMENT,
@@ -158,19 +213,16 @@ CREATE TABLE IF NOT EXISTS `hhpz`
     PRIMARY KEY (`id`),
     KEY `id` (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 967
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  hhpz 的数据：~948 rows (大约)
-DELETE
-FROM `hhpz`;
+-- 正在导出表  mydb1.hhpz 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `hhpz`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `hhpz`
     ENABLE KEYS */;
 
 
--- 导出  表 killc 结构
+-- 导出  表 mydb1.killc 结构
 CREATE TABLE IF NOT EXISTS `killc`
 (
     `id`  bigint(20) NOT NULL,
@@ -179,16 +231,39 @@ CREATE TABLE IF NOT EXISTS `killc`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  killc 的数据：~104 rows (大约)
-DELETE
-FROM `killc`;
+-- 正在导出表  mydb1.killc 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `killc`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `killc`
     ENABLE KEYS */;
 
 
--- 导出  表 person_info 结构
+-- 导出  表 mydb1.notice 结构
+CREATE TABLE IF NOT EXISTS `notice`
+(
+    `id`          int(9)       NOT NULL AUTO_INCREMENT,
+    `state`       int(9)       NOT NULL,
+    `title`       varchar(255) NOT NULL,
+    `icon`        varchar(255) NOT NULL,
+    `date`        varchar(255) NOT NULL,
+    `html`        mediumtext   NOT NULL,
+    `time`        bigint(18)   NOT NULL,
+    `author_name` varchar(255) NOT NULL,
+    `author_id`   bigint(18)   NOT NULL,
+    `views`       int(11)      NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    KEY `id` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+-- 正在导出表  mydb1.notice 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `notice`
+    DISABLE KEYS */;
+/*!40000 ALTER TABLE `notice`
+    ENABLE KEYS */;
+
+
+-- 导出  表 mydb1.person_info 结构
 CREATE TABLE IF NOT EXISTS `person_info`
 (
     `name`          varchar(255) NOT NULL,
@@ -218,7 +293,7 @@ CREATE TABLE IF NOT EXISTS `person_info`
     `mk1`           bigint(18)   NOT NULL,
     `ak1`           bigint(18)   NOT NULL,
     `jak1`          bigint(18)   NOT NULL,
-    `usinged`       varchar(255) NOT NULL DEFAULT 'null',
+    `usinged`       varchar(255) NOT NULL,
     `next_r1`       int(9)       NOT NULL,
     `next_r2`       int(9)       NOT NULL,
     `next_r3`       int(9)       NOT NULL,
@@ -235,16 +310,14 @@ CREATE TABLE IF NOT EXISTS `person_info`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  person_info 的数据：~504 rows (大约)
-DELETE
-FROM `person_info`;
+-- 正在导出表  mydb1.person_info 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `person_info`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `person_info`
     ENABLE KEYS */;
 
 
--- 导出  表 shop_item 结构
+-- 导出  表 mydb1.shop_item 结构
 CREATE TABLE IF NOT EXISTS `shop_item`
 (
     `id`      int(9)     NOT NULL,
@@ -258,16 +331,14 @@ CREATE TABLE IF NOT EXISTS `shop_item`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  shop_item 的数据：~38 rows (大约)
-DELETE
-FROM `shop_item`;
+-- 正在导出表  mydb1.shop_item 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `shop_item`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `shop_item`
     ENABLE KEYS */;
 
 
--- 导出  表 signlist 结构
+-- 导出  表 mydb1.signlist 结构
 CREATE TABLE IF NOT EXISTS `signlist`
 (
     `id`   int(11)     NOT NULL AUTO_INCREMENT,
@@ -277,19 +348,16 @@ CREATE TABLE IF NOT EXISTS `signlist`
     PRIMARY KEY (`id`),
     KEY `id` (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 1876
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  signlist 的数据：~1,751 rows (大约)
-DELETE
-FROM `signlist`;
+-- 正在导出表  mydb1.signlist 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `signlist`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `signlist`
     ENABLE KEYS */;
 
 
--- 导出  表 skill_info 结构
+-- 导出  表 mydb1.skill_info 结构
 CREATE TABLE IF NOT EXISTS `skill_info`
 (
     `qq`          bigint(18)   NOT NULL,
@@ -308,16 +376,14 @@ CREATE TABLE IF NOT EXISTS `skill_info`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  skill_info 的数据：~521 rows (大约)
-DELETE
-FROM `skill_info`;
+-- 正在导出表  mydb1.skill_info 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `skill_info`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `skill_info`
     ENABLE KEYS */;
 
 
--- 导出  表 soul_attribute 结构
+-- 导出  表 mydb1.soul_attribute 结构
 CREATE TABLE IF NOT EXISTS `soul_attribute`
 (
     `wh`          int(9)  NOT NULL,
@@ -332,9 +398,7 @@ CREATE TABLE IF NOT EXISTS `soul_attribute`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  soul_attribute 的数据：~31 rows (大约)
-DELETE
-FROM `soul_attribute`;
+-- 正在导出表  mydb1.soul_attribute 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `soul_attribute`
     DISABLE KEYS */;
 INSERT INTO `soul_attribute` (`wh`, `hp_chance`, `hl_chance`, `hj_chance`, `hp_effect`, `hl_effect`, `hj_effect`,
@@ -374,7 +438,7 @@ VALUES (1, 0, 0, 0, 10, 10, 10, 5),
     ENABLE KEYS */;
 
 
--- 导出  表 soul_bone 结构
+-- 导出  表 mydb1.soul_bone 结构
 CREATE TABLE IF NOT EXISTS `soul_bone`
 (
     `qid`   bigint(18)   NOT NULL,
@@ -385,16 +449,14 @@ CREATE TABLE IF NOT EXISTS `soul_bone`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  soul_bone 的数据：~259 rows (大约)
-DELETE
-FROM `soul_bone`;
+-- 正在导出表  mydb1.soul_bone 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `soul_bone`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `soul_bone`
     ENABLE KEYS */;
 
 
--- 导出  表 task_point 结构
+-- 导出  表 mydb1.task_point 结构
 CREATE TABLE IF NOT EXISTS `task_point`
 (
     `q`              bigint(18) NOT NULL,
@@ -407,40 +469,14 @@ CREATE TABLE IF NOT EXISTS `task_point`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  task_point 的数据：~469 rows (大约)
-DELETE
-FROM `task_point`;
+-- 正在导出表  mydb1.task_point 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `task_point`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `task_point`
     ENABLE KEYS */;
 
 
--- 导出  表 trading_record 结构
-CREATE TABLE IF NOT EXISTS `trading_record`
-(
-    `type0` varchar(255) NOT NULL,
-    `type1` varchar(255) NOT NULL,
-    `from`  bigint(20)   NOT NULL,
-    `to`    bigint(20)   NOT NULL,
-    `main`  bigint(20)   NOT NULL,
-    `now`   int(18)      NOT NULL,
-    `many`  int(18)      NOT NULL,
-    `desc`  varchar(255) NOT NULL,
-    `time`  bigint(20)   NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
-
--- 正在导出表  trading_record 的数据：~16,958 rows (大约)
-DELETE
-FROM `trading_record`;
-/*!40000 ALTER TABLE `trading_record`
-    DISABLE KEYS */;
-/*!40000 ALTER TABLE `trading_record`
-    ENABLE KEYS */;
-
-
--- 导出  表 upup 结构
+-- 导出  表 mydb1.upup 结构
 CREATE TABLE IF NOT EXISTS `upup`
 (
     `qid`   bigint(20) NOT NULL,
@@ -449,44 +485,55 @@ CREATE TABLE IF NOT EXISTS `upup`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  upup 的数据：~1,391 rows (大约)
-DELETE
-FROM `upup`;
+-- 正在导出表  mydb1.upup 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `upup`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `upup`
     ENABLE KEYS */;
 
 
--- 导出  表 user_score 结构
+-- 导出  表 mydb1.user_score 结构
 CREATE TABLE IF NOT EXISTS `user_score`
 (
     `who`       bigint(20) NOT NULL,
-    `day`       int(11)    NOT NULL DEFAULT '0',
-    `days`      int(11)    NOT NULL DEFAULT '0',
-    `fz`        int(11)    NOT NULL DEFAULT '0',
-    `k`         bigint(20) NOT NULL DEFAULT '0',
-    `score`     bigint(20) NOT NULL DEFAULT '0',
-    `s_score`   bigint(20) NOT NULL DEFAULT '0',
-    `times`     int(11)    NOT NULL DEFAULT '0',
-    `times_day` int(11)    NOT NULL DEFAULT '0',
-    `s_times`   int(11)    NOT NULL DEFAULT '0',
+    `day`       int(11)    NOT NULL,
+    `days`      int(11)    NOT NULL,
+    `fz`        int(11)    NOT NULL,
+    `k`         bigint(20) NOT NULL,
+    `score`     bigint(20) NOT NULL,
+    `s_score`   bigint(20) NOT NULL,
+    `times`     int(11)    NOT NULL,
+    `times_day` int(11)    NOT NULL,
+    `s_times`   int(11)    NOT NULL,
     `earnings`  bigint(20) NOT NULL DEFAULT '0',
     `debuffs`   bigint(20) NOT NULL DEFAULT '0',
     UNIQUE KEY `who` (`who`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8 COMMENT ='用户积分相关信息';
 
--- 正在导出表  user_score 的数据：~1,424 rows (大约)
-DELETE
-FROM `user_score`;
+-- 正在导出表  mydb1.user_score 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `user_score`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_score`
     ENABLE KEYS */;
 
 
--- 导出  表 warp 结构
+-- 导出  表 mydb1.verify0_entity 结构
+CREATE TABLE IF NOT EXISTS `verify0_entity`
+(
+    `code`   varchar(255) NOT NULL,
+    `expire` bigint(18)   NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+-- 正在导出表  mydb1.verify0_entity 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `verify0_entity`
+    DISABLE KEYS */;
+/*!40000 ALTER TABLE `verify0_entity`
+    ENABLE KEYS */;
+
+
+-- 导出  表 mydb1.warp 结构
 CREATE TABLE IF NOT EXISTS `warp`
 (
     `id`       bigint(18) DEFAULT NULL,
@@ -497,16 +544,14 @@ CREATE TABLE IF NOT EXISTS `warp`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  warp 的数据：~485 rows (大约)
-DELETE
-FROM `warp`;
+-- 正在导出表  mydb1.warp 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `warp`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `warp`
     ENABLE KEYS */;
 
 
--- 导出  表 zon 结构
+-- 导出  表 mydb1.zon 结构
 CREATE TABLE IF NOT EXISTS `zon`
 (
     `id`    int(9)     NOT NULL,
@@ -518,15 +563,14 @@ CREATE TABLE IF NOT EXISTS `zon`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  zon 的数据：~100 rows (大约)
-DELETE
-FROM `zon`;
+-- 正在导出表  mydb1.zon 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `zon`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `zon`
     ENABLE KEYS */;
 
--- 导出  表 zong 结构
+
+-- 导出  表 mydb1.zong 结构
 CREATE TABLE IF NOT EXISTS `zong`
 (
     `name`      varchar(255) NOT NULL,
@@ -548,21 +592,7 @@ CREATE TABLE IF NOT EXISTS `zong`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- 正在导出表  zong 的数据：~7 rows (大约)
-DELETE
-FROM `zong`;
-CREATE TABLE IF NOT EXISTS all_message
-(
-    `time`        BIGINT(18)   NOT NULL,
-    `id`          INT(9)       NOT NULL,
-    `internal_id` INT(9)       NOT NULL,
-    `sender_id`   BIGINT(18)   NOT NULL,
-    `bot_id`      BIGINT(18)   NOT NULL,
-    `type`        VARCHAR(255) NOT NULL,
-    `from_id`     BIGINT(18)   NOT NULL,
-    `content`     VARCHAR(255) NOT NULL,
-    `recalled`    INT(9)       NOT NULL
-);
+-- 正在导出表  mydb1.zong 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `zong`
     DISABLE KEYS */;
 /*!40000 ALTER TABLE `zong`
@@ -570,5 +600,3 @@ CREATE TABLE IF NOT EXISTS all_message
 /*!40101 SET SQL_MODE = IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS = IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-
-
